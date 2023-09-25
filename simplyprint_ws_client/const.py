96986c20fd903b413e2964c8baf9d69d03fe9281
Javad Asgari Shafique <me@javad.sh>
2023-09-25 19:32:36 +0200

    Allow for custom ws urls

--- simplyprint_ws_client/const.py
+++ simplyprint_ws_client/const.py
@@ -3,5 +3,6 @@ from os import environ
 VERSION = "1.0.0-rc.1"
 IS_TESTING = bool(environ.get("IS_TESTING"))
 API_VERSION = "0.1"
-API_URL = "https://api.simplyprint.io" if not IS_TESTING else "https://apirewrite.simplyprint.io"
+API_URL = environ.get("API_URL", "https://api.simplyprint.io" if not IS_TESTING else "https://apirewrite.simplyprint.io")
+
 WEBSOCKET_URL = "wss://ws.simplyprint.io" if not IS_TESTING else "wss://testws2.simplyprint.io"
