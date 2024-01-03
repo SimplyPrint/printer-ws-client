1b145e7bfb12b792724b1d5c4381507697fdf984
Javad Asgari Shafique <me@javad.sh>
2024-01-03 21:19:39 +0100

    Improve send_logs event with more concrete hints for log files to send

--- simplyprint_ws_client/events/demands.py
+++ simplyprint_ws_client/events/demands.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict, List, Optional, Union
+
 from .server_events import ServerEvent
+
 
 class DemandEvent(ServerEvent):
     event_type = "demand"
@@ -20,14 +22,18 @@ class DemandEvent(ServerEvent):
 
         return cls.demand or cls.event_type
 
+
 class PauseEvent(DemandEvent):
     demand = "pause"
+
 
 class ResumeEvent(DemandEvent):
     demand = "resume"
 
+
 class CancelEvent(DemandEvent):
     demand = "cancel"
+
 
 class TerminalEvent(DemandEvent):
     demand = "terminal"
@@ -35,21 +41,25 @@ class TerminalEvent(DemandEvent):
     def on_event(self):
         self.enabled: bool = self.data.get("enabled", False)
 
+
 class GcodeEvent(DemandEvent):
     demand = "gcode"
 
     def on_event(self):
         self.list: List[str] = self.data.get("list", [])
 
+
 class WebcamTestEvent(DemandEvent):
     demand = "webcam_test"
 
+
 class WebcamSnapshotEvent(DemandEvent):
     demand = "webcam_snapshot"
-    
+
     def on_event(self):
         self.id: Optional[str] = self.data.get("id")
         self.timer: Optional[int] = self.data.get("timer")
+
 
 class FileEvent(DemandEvent):
     demand = "file"
@@ -65,42 +75,56 @@ class FileEvent(DemandEvent):
 
         # Convert mapping to list of integers where -1 is None
         mms_map = self.data.get("mms_map", None)
-        self.mms_map: Optional[List[int]] = list(map(lambda n: -1 if n is None else n, mms_map)) if mms_map is not None else None
+        self.mms_map: Optional[List[int]] = list(
+            map(lambda n: -1 if n is None else n, mms_map)) if mms_map is not None else None
+
 
 class StartPrintEvent(DemandEvent):
     demand = "start_print"
 
+
 class ConnectPrinterEvent(DemandEvent):
     demand = "connect_printer"
+
 
 class DisconnectPrinterEvent(DemandEvent):
     demand = "disconnect_printer"
 
+
 class SystemRestartEvent(DemandEvent):
     demand = "system_restart"
+
 
 class SystemShutdownEvent(DemandEvent):
     demand = "system_shutdown"
 
+
 class ApiRestartEvent(DemandEvent):
     demand = "api_restart"
+
 
 class ApiShutdownEvent(DemandEvent):
     demand = "api_shutdown"
 
+
 class UpdateEvent(DemandEvent):
     demand = "update"
+
 
 class PluginInstallEvent(DemandEvent):
     demand = "plugin_install"
 
+
 class PluginUninstallEvent(DemandEvent):
     demand = "plugin_uninstall"
 
+
 class WebcamSettingsEvent(DemandEvent):
     demand = "webcam_settings_updated"
+
     def on_event(self):
         self.webcam_settings = self.data.get("webcam_settings")
+
 
 # deprecated
 class StreamOnEvent(DemandEvent):
@@ -109,9 +133,11 @@ class StreamOnEvent(DemandEvent):
     def on_event(self):
         self.interval: float = self.data.get("interval", 300.0) / 1000.0
 
+
 # deprecated
 class StreamOffEvent(DemandEvent):
     demand = "stream_off"
+
 
 class SetPrinterProfileEvent(DemandEvent):
     demand = "set_printer_profile"
@@ -119,11 +145,13 @@ class SetPrinterProfileEvent(DemandEvent):
     def on_event(self):
         self.profile = self.data.get("printer_profile")
 
+
 class GetGcodeScriptBackupsEvent(DemandEvent):
     demand = "get_gcode_script_backups"
 
     def on_event(self):
         self.force = self.data.get("force", False)
+
 
 class HasGcodeChangesEvent(DemandEvent):
     demand = "has_gcode_changes"
@@ -131,11 +159,13 @@ class HasGcodeChangesEvent(DemandEvent):
     def on_event(self):
         self.scripts = self.data.get("scripts")
 
+
 class PsuControlEvent(DemandEvent):
     demand = "psu_keepalive"
 
     def on_event(self):
         self.on: bool = True
+
 
 class PsuOnControlEvent(PsuControlEvent):
     demand = "psu_on"
@@ -143,17 +173,20 @@ class PsuOnControlEvent(PsuControlEvent):
     def on_event(self):
         self.on: bool = True
 
+
 class PsuOffControlEvent(PsuControlEvent):
     demand = "psu_off"
 
     def on_event(self):
         self.on: bool = False
-    
+
+
 class DisableWebsocketEvent(DemandEvent):
     demand = "disable_websocket"
 
     def on_event(self):
         self.websocket_ready: bool = self.data.get("websocket_ready", False)
+
 
 class SendLogsEvent(DemandEvent):
     demand = "send_logs"
@@ -162,3 +195,7 @@ class SendLogsEvent(DemandEvent):
         self.token: str = self.data.get("token", "")
         self.logs: List[str] = self.data.get("logs", "")
         self.max_body = self.data.get("max_body", 100000000)
+
+        self.send_main: bool = "main" in self.logs
+        self.send_plugin: bool = "plugin_log" in self.logs
+        self.send_serial: bool = "serial_log" in self.logs
