4d9cfe1882185bf0b1c5965d61666908df015f82
Javad Asgari Shafique <me@javad.sh>
2024-02-25 15:42:04 +0100

    Formatting

--- simplyprint_ws_client/helpers/ambient_check.py
+++ simplyprint_ws_client/helpers/ambient_check.py
@@ -9,12 +9,16 @@ from ..state import ClientState, to_event
 
 class AmbientCheck:
     AMBIENT_CHECK_TIME = 5.0 * 60.0
-    SAMPLE_CHACK_TIME = 20.0
+    SAMPLE_CHECK_TIME = 20.0
     CHECK_INTERVAL = 5.0
 
     @staticmethod
-    def detect(on_changed: Callable[[int], None], tools: List[Temperature], initial_sample: Optional[float] = None,
-               ambient: float = 0) -> Tuple[Optional[float], int, float]:
+    def detect(
+            on_changed: Callable[[int], None],
+            tools: List[Temperature],
+            initial_sample: Optional[float] = None,
+            ambient: float = 0
+    ) -> Tuple[Optional[float], int, float]:
         if len(tools) == 0:
             return None, round(ambient), AmbientCheck.CHECK_INTERVAL
 
@@ -24,16 +28,19 @@ class AmbientCheck:
             return None, round(ambient), AmbientCheck.AMBIENT_CHECK_TIME
 
         if initial_sample is None:
-            return tool0.actual, round(ambient), AmbientCheck.SAMPLE_CHACK_TIME
-        else:
-            diff = abs(tool0.actual - initial_sample)
-            if diff <= 2.0:
-                ambient = (tool0.actual + initial_sample) / 2
-                if ambient != ambient:
-                    on_changed(round(ambient))
-                return None, round(ambient), AmbientCheck.AMBIENT_CHECK_TIME
-            else:
-                return tool0.actual, round(ambient), AmbientCheck.SAMPLE_CHACK_TIME
+            return tool0.actual, round(ambient), AmbientCheck.SAMPLE_CHECK_TIME
+
+        diff = abs(tool0.actual - initial_sample)
+
+        if diff <= 2.0:
+            ambient = (tool0.actual + initial_sample) / 2
+
+            if ambient != ambient:
+                on_changed(round(ambient))
+
+            return None, round(ambient), AmbientCheck.AMBIENT_CHECK_TIME
+
+        return tool0.actual, round(ambient), AmbientCheck.SAMPLE_CHECK_TIME
 
 
 @to_event(AmbientTemperatureEvent, "ambient")
@@ -47,10 +54,17 @@ class AmbientTemperatureState(ClientState):
 
     def invoke_check(self, tool_temperatures: List[Temperature]):
         """
-        It is up to the implementation to decide when to invoke the check or respect the update_interval, the entire state is self contained
-        and requires the tool_temperatures to be passed in from the PrinterState, but it handles triggering the appropriate events.
+        It is up to the implementation to decide when to invoke the check or respect the update_interval,
+        the entire state is self-contained and requires the tool_temperatures to be passed in from the PrinterState,
+        but it handles triggering the appropriate events.
         """
-        (self.initial_sample, self.ambient, self.update_interval) = AmbientCheck.detect(self.on_changed_callback,
-                                                                                        tool_temperatures,
-                                                                                        self.initial_sample,
-                                                                                        self.ambient)
+        (
+            self.initial_sample,
+            self.ambient,
+            self.update_interval
+        ) = AmbientCheck.detect(
+            self.on_changed_callback,
+            tool_temperatures,
+            self.initial_sample,
+            self.ambient
+        )
