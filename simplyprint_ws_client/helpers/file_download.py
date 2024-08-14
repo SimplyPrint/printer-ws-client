0c61d4a4fb40342b629525aef242cca6eed2ebdd
Tim Schneider <tim@schneider.engineering>
2024-08-14 19:16:30 +0200

    [feature] add timeout parameter to class FileDownload (#9)

--- simplyprint_ws_client/helpers/file_download.py
+++ simplyprint_ws_client/helpers/file_download.py
@@ -11,10 +11,19 @@ from ..client.state import PrinterFileProgressState, FileProgressState
 class FileDownload:
     state: "PrinterFileProgressState"
     client: Client
+    timeout: aiohttp.ClientTimeout
 
-    def __init__(self, client: Client) -> None:
+    def __init__(self, client: Client, timeout: Optional[aiohttp.ClientTimeout] = None) -> None:
         self.client = client
         self.state = client.printer.file_progress
+
+        self.timeout = timeout or aiohttp.ClientTimeout(
+            # default is total = 5 minutes which is too short for large files
+            total=None,  # Total number of seconds for the whole request
+            connect=5,  # Maximal number of seconds for acquiring a connection from pool
+            sock_connect=10,  # Maximal number of seconds for connecting to a peer for a new connection
+            sock_read=60*30  # seconds for consecutive reads - 30 minutes as we do not control the block size
+        )
 
     async def download(self, url, clamp_progress: Optional[Callable] = None) -> AsyncIterable:
         """ 
@@ -22,7 +31,7 @@ class FileDownload:
         """
 
         # Chunk the download so we can get progress
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(timeout=self.timeout) as session:
             async with session.get(url) as resp:
                 if resp.status != 200:
                     self.state.state = FileProgressState.ERROR
